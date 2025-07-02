c5c009889fce0e697762b8f5926fbe4d288022ab
Michel Policeno <michel.policeno@hotmail.com>
2025-07-02 16:39:51 -0300

    feat cnpj: ajuste para conseguir escolher qual chamar

--- src/index.ts
+++ src/index.ts
@@ -7,62 +7,54 @@ import { validaCpf } from "./cpf/validarCpf.js";
 import { geradorCNPJ } from "./cnpj/geradorCNPJ.js";
 import { validaCNPJ } from "./cnpj/validarCNPJ.js";
 import { criarExcel } from "./excel/salvarPlanilha.js";
-//cpf
-// async function main(): Promise<void> {
-//   const browser = await chromium.launch({ headless: false });
-//   const page = await browser.newPage();
-//   const resultado: string[][] = [];
+import { Page } from "playwright";
 
-//   while (resultado.length < 2) {
-//     try {
-//       const cpfGerado = await geradorCPF(page);
-//       const valido = await validaCpf(page, cpfGerado);
-//       if (valido) {
-//         resultado.push([cpfGerado]);
-//         console.log(`CPF ${cpfGerado} - sucesso`);
-//       }
-//     } catch (err) {
-//       console.error(`Erro ao gerar CPF. Erro: ${err}`);
-//     }
-//     //verificar se a pagina continua Aberta - evitando laço infinito de erro
-//     if (page.isClosed()) {
-//       console.log("A página foi fechada pelo usuário. Encerrando o processo.");
-//       break;
-//     }
-//   }
-//   await browser.close();
+//paramentros
+let tipoConsulta = "CPF"; //CPF ou CNPJ
+const quantidadeDesejada = 5;
 
-//   try {
-//     criarExcel(resultado);
-//   } catch (err) {
-//     console.error(`Erro ao criar Excel. Erro: ${err}`);
-//   }
-// }
-
-async function main(): Promise<void> {
-  const browser = await chromium.launch({
-    headless: false,
-    channel: "chrome", // usa o Chrome real instalado na máquina
-  });
-  const page = await browser.newPage();
-  const resultado: string[][] = [];
-
-  while (resultado.length < 2) {
+async function gerarDocumentos(
+  page: Page,
+  resultado: string[][],
+  gerarDoc: Function,
+  validaDoc: Function
+) {
+  while (resultado.length < quantidadeDesejada) {
     try {
-      const cnpjGerado = await geradorCNPJ(page);
-      const valido = await validaCNPJ(page, cnpjGerado);
+      const docGerado = await gerarDoc(page);
+      const valido = await validaDoc(page, docGerado);
       if (valido) {
-        resultado.push([cnpjGerado]);
-        console.log(`cnpj ${cnpjGerado} - sucesso`);
+        resultado.push([docGerado]);
+        console.log(`${docGerado} obteve sucesso`);
       }
     } catch (err) {
-      console.error(`Erro ao gerar cnpj. Erro: ${err}`);
+      console.error(`Erro ao gerar doc. Erro: ${err}`);
     }
     //verificar se a pagina continua Aberta - evitando laço infinito de erro
     if (page.isClosed()) {
       console.log("A página foi fechada pelo usuário. Encerrando o processo.");
       break;
     }
+  }
+}
+
+async function main(): Promise<void> {
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
+  const resultado: string[][] = [];
+
+  switch (tipoConsulta) {
+    case "CPF":
+      await gerarDocumentos(page, resultado, geradorCPF, validaCpf);
+      break;
+
+    case "CNPJ":
+      await gerarDocumentos(page, resultado, geradorCNPJ, validaCNPJ);
+      break;
+
+    default:
+      console.log("Escolha entre CPF ou CNPJ");
+      break;
   }
   await browser.close();
 
