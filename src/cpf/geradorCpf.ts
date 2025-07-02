3ee0a7cf46e67910cb79e78e17f1232b6233a2bb
Michel Policeno <michel.policeno@hotmail.com>
2025-07-02 11:57:55 -0300

    refactor: separação dos seletores usados em cada pagina

--- src/cpf/geradorCpf.ts
+++ src/cpf/geradorCpf.ts
@@ -1,16 +1,17 @@
 import { Page } from "playwright";
+import seletores from "../selectors/paginas.json" with { type: "json" };
 
 export async function geradorCPF(page: Page): Promise<string> {
-  await page.goto("https://www.4devs.com.br/gerador_de_cpf", {
+  await page.goto(seletores['4devs'].url, {
     waitUntil: "domcontentloaded",
   });
   //Seleciona checkbox - "cpf com pontuação" como não
-  await page.click("#pontuacao_nao");
+  await page.click(seletores["4devs"].semPontuacao);
   await page.waitForTimeout(100);
 
   //Click botão para gerar cpf
-  await page.click("#bt_gerar_cpf");
+  await page.click(seletores["4devs"].botaoGerarCpf);
   await page.waitForTimeout(1500);
 
-  return (await page.textContent("#texto_cpf")) || "";
+  return (await page.textContent(seletores["4devs"].campoCpfGerado)) || "";
 }
